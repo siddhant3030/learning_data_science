9a1429bfd573ad98a81c9db8c12927033e08f04f
siddhant <ssiddhant3030@gmail.com>
2023-02-12 18:27:42 +0530

    zip func

---
+++ Untitled5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarr = np.arange(26)
+mydict = dict(zip(mylist, myarr))
+
+
+# In[2]:
+
+
+mydict
+
+
+# In[ ]:
+
+
+
+
